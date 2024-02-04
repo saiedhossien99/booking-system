29f7c2bff1270c34fc43a14ed34cf4ec015e16b6
saied hossien <saiedhossien99@gmail.com>
2024-02-05 05:45:34 +0600

    header dialouge bar add

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
-
+import { trending_data } from "@/data/TrendingData";
 export default function Home() {
   return (
     <div>
-      hello page 
+          <div className="flex space-x-4 py-5 overflow-x-scroll">
+          {trending_data.map((item) => (
+            <div key={item.id} className="space-y-1 shrink-0 cursor-pointer">
+              <img
+                key={item.id}
+                className="w-80 h-72 object-cover rounded-lg pb-2"
+                src={item.src}
+                alt=""
+              />
+
+              <p className="font-bold">{item.title}</p>
+              <p className="">{item.location}</p>
+              <p className="font-light text-sm">{item.description}</p>
+            </div>
+          ))}
+        </div>
     </div>
   );
 }
