5cb5e8e2334d9a23bd80e08bc196a7a5979865dc
saied hossien <saiedhossien99@gmail.com>
2024-02-07 04:31:08 +0600

    main page modification

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,29 @@
 import { trending_data } from "@/data/TrendingData";
+
 export default function Home() {
   return (
-    <div>
-          <div className="flex space-x-4 py-5 overflow-x-scroll">
+    <main className="bg-[#013B94]">
+      <section className="max-w-7xl mx-auto p-6">
+        <h2 className="font-bold text-5xl text-white">Find your Next Stay</h2>
+        <h3 className="text-white py-5 text-xl">
+          Search low prices on hotels, homes and much more...
+        </h3>
+      </section>
+
+      <section className="mx-auto max-w-7xl mt-10 p-6 bg-white rounded-t-[8px] ">
+        <div className="pt-5">
+          <h3 className="text-xl font-bold">Trending Destinations</h3>
+          <p className="font-light">
+            Most popular choices for travellers from around the world
+          </p>
+        </div>
+
+        <div className="flex space-x-4 py-5 overflow-x-scroll">
           {trending_data.map((item) => (
             <div key={item.id} className="space-y-1 shrink-0 cursor-pointer">
               <img
                 key={item.id}
-                className="w-80 h-72 object-cover rounded-lg pb-2"
+                className="w-80 h-72 object-cover rounded-[8px] pb-2"
                 src={item.src}
                 alt=""
               />
@@ -18,6 +34,7 @@ export default function Home() {
             </div>
           ))}
         </div>
-    </div>
+      </section>
+    </main>
   );
 }
